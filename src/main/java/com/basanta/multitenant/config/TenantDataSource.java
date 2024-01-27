8838359d56723b37fb40fa4cef5207f49a3fc95a
Basanta.thapa <basanta.thapa@infodev.com.np>
2024-01-27 15:18:04 +0545

    feat: Data Source

--- src/main/java/com/basanta/multitenant/config/TenantDataSource.java
+++ src/main/java/com/basanta/multitenant/config/TenantDataSource.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
-import javax.xml.crypto.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 
